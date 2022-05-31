6c1b07b25abcc8f2acbae56a0bbea087c8c1bb93
Lycano91 <106607053+Lycano91@users.noreply.github.com>
2022-05-31 17:50:32 +0200

    Update main.java

---
+++ java/main.java
@@ -0,0 +1,12 @@
+package java;
+
+
+public class main {
+
+    public static void main(String[] args) {
+        double g = Math.random() * 10;
+        g = 0 ;
+        System.out.println(g);
+
+    }   
+}

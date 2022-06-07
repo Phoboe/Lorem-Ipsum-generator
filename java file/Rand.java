c7abbe62ce2d1d4dbb498dfa359366dd084e05f4
Lycano91 <106607053+Lycano91@users.noreply.github.com>
2022-06-07 19:08:47 +0200

    Modification du programme de random

--- java file/Rand.java
+++ java file/Rand.java
@@ -1,10 +1,7 @@
 public class Rand {
-    public static int rand(){
-        double g;
-        int h;
-        g = Math.random() * 10;
-        h = (int)g ;
-        System.out.println(h);
+    public static int rand(int min , int max){
+        int range = max - min ,
+        h = (int)(Math.random() * range) + min ;
         return h;
     }
 }
--- java file/main.java
+++ java file/main.java
@@ -1,25 +1,17 @@
+import java.util.Scanner;
+
 public class main {
 
     public static void main(String[] args) {
-        int maxmot = 10;
-        int taille = 10;
-        int rando , x , y , z;
-        int nb_de_mot = 1000000;
-        String[] dico = new String[]{"he","ha","hi"};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Rentrez le nombre de mot :");
+        int maxmot = scanner.nextInt() , rando;
+        String[] liste_mot = new String[]{"he","ha","hi"};
         String produit_fini = new String();
         
-        rando = Rand.rand();
         for( int i = 0 ; i < maxmot ; i++ ){
-            if(rando <= 3) {
-                produit_fini += dico[0] + " ";
-            }
-            else if (rando > 3 && rando <= 6){
-                produit_fini += dico[1] + " ";
-            }
-            else if (rando > 6 && rando <= 10){
-                produit_fini += dico[2] + " ";
-            }
-            rando = Rand.rand();
+            rando = Rand.rand(0, liste_mot.length);
+            produit_fini += liste_mot[rando]+" ";
         }
 
         System.out.println(produit_fini);

bbb8290ab933d32025de861d669df8ae91a14759
Lycano91 <106607053+Lycano91@users.noreply.github.com>
2022-06-07 19:32:27 +0200

    Update main.java

--- java file/main.java
+++ java file/main.java
@@ -1,3 +1,5 @@
+import java.io.*;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class main {
@@ -8,7 +10,20 @@ public class main {
         int maxmot = scanner.nextInt() , rando;
         String[] liste_mot = new String[]{"he","ha","hi"};
         String produit_fini = new String();
+
+        try (BufferedReader br = new BufferedReader(new FileReader("un fichier.csv"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+            }
+            
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
         
+
         for( int i = 0 ; i < maxmot ; i++ ){
             rando = Rand.rand(0, liste_mot.length);
             produit_fini += liste_mot[rando]+" ";

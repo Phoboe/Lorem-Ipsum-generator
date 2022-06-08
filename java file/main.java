f9147060559dc9329986be273e92cda1a14826b0
Lycano91 <106607053+Lycano91@users.noreply.github.com>
2022-06-08 16:39:18 +0200

    Tout fonctionnel(je crois)

--- java file/main.java
+++ java file/main.java
@@ -1,5 +1,7 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class main {
@@ -8,25 +10,32 @@ public class main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Rentrez le nombre de mot :");
         int maxmot = scanner.nextInt() , rando;
-        String[] liste_mot = new String[]{"he","ha","hi"};
+        List liste_mot = new ArrayList();
         String produit_fini = new String();
 
-        try (BufferedReader br = new BufferedReader(new FileReader("un fichier.csv"))) {
+        List<List<String>> records = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("python/LoremIpsum.csv"))) {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] values = line.split(",");
+                records.add(Arrays.asList(values));
             }
-            
         }
-        catch (Exception e) {
-            e.printStackTrace();
+        catch (Exception e){
         }
 
+        //System.out.println(records);
         
+        for (int i = 0 ; i < records.size() ; i++ ){
+            for (int j = 0 ; j < records.get(i).size() ; j++ ){
+                liste_mot.add(records.get(i).get(j));
+            }
+        }
+
 
         for( int i = 0 ; i < maxmot ; i++ ){
-            rando = Rand.rand(0, liste_mot.length);
-            produit_fini += liste_mot[rando]+" ";
+            rando = Rand.rand(0, liste_mot.size());
+            produit_fini += liste_mot.get(rando)+" ";
         }
 
         System.out.println(produit_fini);

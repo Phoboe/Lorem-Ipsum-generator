9cf073f226ee473bbe9f6deb56ecfa4380e80e5d
Phoboe <contactatnina@gmail.com>
2022-06-20 15:47:16 +0200

    Changed csv export to double entry table

--- python/main.py
+++ python/main.py
@@ -8,6 +8,7 @@ Docstring for main.py :
 
 inputFileName = 'LoremIpsum.txt'
 # inputFileName = 'little text.txt' # to test count_next_words function
+# inputFileName = 'lultime experience.txt' # french big text
 outputFileName = 'LoremIpsum.csv'
 
 import os
@@ -30,7 +31,7 @@ def get_lorem_ipsum(fileName):
 
     try: # If the file already exists
 
-        with open(fileName, mode='r') as file:
+        with open(fileName, mode='r', encoding="utf8") as file:
             array = file.readlines()
 
     except FileNotFoundError:
@@ -56,6 +57,26 @@ def get_words(string):
     ',',
     ';',
     '\n',
+    's’',
+    'm’',
+    'l’',
+    'd’',
+    'qu’',
+    '>',
+    '<',
+    '...',
+    '(',
+    ')',
+    '{',
+    '}',
+    '[',
+    ']',
+    '\\',
+    '/',
+    '+',
+    '$',
+    '      ',
+    '     ',
     '    ',
     '   ',
     '  ',
@@ -96,6 +117,7 @@ def count_next_words(words):
         result.append(couples) # result is now constructed but with empty values
 
     print(f'Finding next words : ')
+    state = 0
     for k in range(len(words)-1): # read the text
         word = words[k]
         next = words[k+1]
@@ -110,11 +132,13 @@ def count_next_words(words):
                     if current == word and next == couple[0]: # found a next word corresponding
                         # print(f'  Found a corresponding word : {next} is after {current}.')
                         result[i][j][1] += 1 # add an occurence for this word
-        percentage = int((100 * (k/len(words)))*100)/100
-        # os.system('cls')
-        if percentage % 1 == 0.0:
-            print(f'Progression : {int(percentage)}%.')
 
+        state += 1
+        if state % (len(words)/100) == 0:
+            percentage = 100 * (k/len(words))
+            print(f'Progression : {round(percentage, 2)}%')
+        pass
+    print('Progression : 100%. END')
 
 
     print('\n')
@@ -127,6 +151,59 @@ def count_next_words(words):
 
     return result
 
+def count_next_words2(words):
+    """ Creates a list with all the words and the chance for each word to be written next.
+        text is a list of all the words of the text in order """
+
+    parsed_words = remove_duplicates(words)
+
+    result = [] # list of lists containing the couples of words and their probability to be next to another
+
+    columns = ['']
+    for word in parsed_words: # set the columns names
+        columns.append(word)
+    result.append(columns)
+
+    for parsed in parsed_words: # initialising values with 0
+        couples = [parsed]
+        for word in parsed_words:
+            value = 0
+            couples.append(value)
+            continue
+        result.append(couples) # result is now constructed but with empty values
+
+    print(f'\nFinding next words : ')
+    state = 0
+    for i in range(len(words)-1): # read the text
+        word = words[i]
+        next = words[i+1]
+        for x in range(len(result)):
+            current = result[x][0]
+            for y in range(len(result[x])):
+                if x >= 1: # ignore the first element because its not a couple but the current word
+                    if current == word and next == result[0][y]: # found a next word corresponding
+                        result[x][y] += 1 # add an occurence for this word
+        percentage = int((100 * (i/len(words)))*100)/100
+
+        state += 1
+        if state == 10:
+            state = 0
+            percentage = 100 * (i/len(words))
+            print(f'Progression : {round(percentage, 3)}%')
+        pass
+
+    print('Progression : 100%. END')
+
+
+    print('\n')
+    print_list(result)
+    print('\nScript readable string : ')
+    print(result)
+
+
+
+
+    return result
 
 def export(array, fileName):
     with open(fileName, mode='w', newline='') as file:
@@ -144,14 +221,15 @@ lines  = get_lorem_ipsum(inputFileName)
 string = list_to_string(lines)
 words  = get_words(string)
 
-print(f'\nTotal {len(words)} words. \n')
 
 print('-'*15, '\nSorted words by lenght :\n')
-print_list(sort_by_lenght(remove_duplicates(words)))
+no_duplicates = sort_by_lenght(remove_duplicates(words))
+print_list(no_duplicates)
+print(f'\nTotal {len(words)} words in the text with {len(no_duplicates)} diferents words. \n')
 
 import time
 start_time = time.time()
-probability = count_next_words(words)
+probability = count_next_words2(words)
 seconds = time.time() - start_time
 print(f'\n --- count_next_words() took {seconds} to execute. ---')
 
